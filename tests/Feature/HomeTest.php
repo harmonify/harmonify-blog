40168f477b7b622304b47c5b3333fb38034929c5
Wendy Surya Wijaya <wendy18.sw@gmail.com>
2021-09-24 23:39:46 +0700

    add render home test

---
+++ tests/Feature/HomeTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HomeTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_index()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_about()
+    {
+        return $this->get('/about')->assertStatus(200);
+    }
+}
